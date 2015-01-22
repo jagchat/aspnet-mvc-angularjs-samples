4a69ba64271abc0b01cd39e392afa406e0620532
Jag <jag.chat@gmail.com>
2015-01-22 14:30:00 -0600

    Added 01/06

---
+++ 01-Introduction/06-A simple Angular Service (using 'factory' method)/README.md
@@ -0,0 +1,7 @@
+# Samples using AngularJS and ASP.NET MVC
+
+## AngularJS: A simple angular service (using 'factory')
+
+* Demonstrates on creating a new service
+* Demonstrates on passing a service to controller
+* Demonstrates on accessing a service (and calling a service method) from a controller
---
+++ 01-Introduction/06-A simple Angular Service (using 'factory' method)/SampleAppSol/SampleApp/Scripts/Site.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+var App = angular.module('SampleApp', []);
+
+App.controller('EmpCtrl', ['$scope', 'EmpCalcSvc', function ($scope, EmpCalcSvc) {
+    $scope.Empno = 1001;
+    $scope.Ename = "Jag";
+    $scope.Sal = 4500;
+    $scope.Deptno = 20;
+    $scope.GetAnnSal = function () {
+        return EmpCalcSvc.GetAnnSal($scope.Sal);
+    };
+}]);
+
+App.factory('EmpCalcSvc', function () {
+    var singletonInstance = {};
+    singletonInstance.GetAnnSal = function (salary) {
+        return salary * 12;
+    };
+    return singletonInstance;
+});
